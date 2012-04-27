040f1bb665807e602aefc5fdded9f099d140e5e5
Norton Wang <nortantivirus@gmail.com>
2012-04-27 18:17:22 -0400

    zzz

--- mmdjango/settings.py
+++ mmdjango/settings.py
@@ -121,7 +121,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mm',
-    'gunicorn',
     # Uncomment the next line to enable the admin:
      'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
